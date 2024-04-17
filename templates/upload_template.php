53bc5a6d0f618fdc6b3cea2fcd100dde790cd509
Alberto <puggioni.alberto@gmail.com>
2024-04-17 09:42:47 +0200

    revisione commenti

--- templates/upload_template.php
+++ templates/upload_template.php
@@ -1,27 +1,29 @@
 <?php
-session_start(); // Avvia la sessione
+// Avvia la sessione
+session_start();
 global $conn;
+
 // Connessione al database
 include("../modules/connection_db.php");
 
-// Verifica se l'utente è autenticato
+// Verifica se l'utente è autenticato per mezzo dell'id
 if(isset($_SESSION['user_id'])) {
-    $user_id = $_SESSION['user_id']; // ID dell'utente attualmente autenticato
+    $user_id = $_SESSION['user_id'];
 
     // Verifica se è stato inviato un ID del template per il publish
     if(isset($_GET['id'])) {
-        $template_id = $_GET['id']; // ID del template selezionato dall'utente
+        $template_id = $_GET['id'];
 
-        // Query per recuperare l'HTML e il CSS dal database per il template selezionato dall'utente
+        // Recupera l'HTML e il CSS dal db per il template selezionato dall'utente
         $fetch_query = "SELECT html, css FROM templates WHERE id=? AND user_id=?";
         $fetch_stmt = $conn->prepare($fetch_query);
         $fetch_stmt->bind_param("ii", $template_id, $user_id);
         $fetch_stmt->execute();
         $template_result = $fetch_stmt->get_result();
 
-        // Verifica se è stato trovato il template selezionato dall'utente
+        // Se è stato trovato un template, allora...
         if ($template_result->num_rows > 0) {
-            // Ottieni il contenuto HTML e CSS dal risultato della query
+            // ... ottiene il contenuto HTML e CSS dal risultato della query
             $template_row = $template_result->fetch_assoc();
             $html_content = stripslashes($template_row['html']);
             $css_content = stripslashes($template_row['css']);
@@ -40,7 +42,7 @@ if(isset($_SESSION['user_id'])) {
             </body>
             </html>";
 
-            // Percorso della cartella da creare
+            // Percorso della cartella da creare --> TODO: creare una cartella generale per ogni singolo utente al cui interno ci sono n dir diverse in base al numero dei template fatti
             $folderPath = '../../pluto';
 
             // Percorso dei file HTML e CSS da creare
@@ -48,12 +50,13 @@ if(isset($_SESSION['user_id'])) {
             $cssPath = $folderPath . '/style.css';
 
             // Verifica se la cartella non esiste già
+            // TODO --> implementare la funzionalità di sovrascrivere eventualmente una dir già creata in base al contenuto
             if (!is_dir($folderPath)) {
                 // Crea la cartella
                 if (mkdir($folderPath, 0777, true)) {
-                    // Scrivi il contenuto HTML nel file index.html
+                    // contenuto HTML nel file index.html
                     if (file_put_contents($indexPath, $html) !== false) {
-                        // Scrivi il contenuto CSS nel file style.css
+                        // Scrive il contenuto CSS nel file style.css
                         if (file_put_contents($cssPath, $css_content) !== false) {
                             echo "I file HTML e CSS sono stati creati con successo.";
                         } else {
@@ -72,14 +75,14 @@ if(isset($_SESSION['user_id'])) {
             echo "Nessun template trovato per l'utente attualmente autenticato con l'ID specificato.";
         }
 
-        // Chiudi il risultato della query
+        // Chiude il risultato della query
         $fetch_stmt->close();
     } else {
         echo "ID del template per il publish non specificato.";
     }
 } else {
-    // Se l'utente non è autenticato, reindirizzalo alla pagina di login
-    header("Location: login.php");
+    // Se l'utente non è autenticato, viene reindirizzato alla pagina di login
+    header("Location: main.php");
     exit();
 }
 
